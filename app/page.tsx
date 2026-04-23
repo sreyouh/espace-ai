fddb9fd523c688f7adc8955d2d3e2746e2b85d2c
Sreehari S <sreeharisunil100@gmail.com>
2026-04-23 13:46:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,21 @@
+"use client";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import BenefitsSection from "../components/BenefitsSection";
+import WhyPortfolioSection from "../components/WhyPortfolioSection";
+import PricingSection from "../components/PricingSection";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <HeroSection />
+      <BenefitsSection />
+      <WhyPortfolioSection />
+      <PricingSection />
+      <Footer />
+    </main>
+  );
+}
 
